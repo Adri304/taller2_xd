ac88dae1f3aec66af52f932f671d60c4d8afd64b
Adri304 <jbunay661@soyudemedellin.edu.co>
2026-04-08 21:43:35 -0500

    Creación y modificación de Scripts
    En la carpeta All_Scripts

--- Taller2/Assets/_Main/All_Scrpits/Trap.cs
+++ Taller2/Assets/_Main/All_Scrpits/Trap.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Interactive : MonoBehaviour
+public class Trap : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
