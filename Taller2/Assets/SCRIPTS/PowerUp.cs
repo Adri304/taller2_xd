d33b281bf649798dab4d9c461d66356536e6c297
imdarlings <dhinestroza880@soyudemedellin.edu.co>
2026-04-12 09:57:43 -0500

    POWER UPS ARREGLO 2
    Crear sistema de recolección de power-ups mediante colisión con aplicación de efectos lógicos al jugador

--- Taller2/Assets/SCRIPTS/PowerUp.cs
+++ Taller2/Assets/SCRIPTS/PowerUp.cs
@@ -8,4 +8,31 @@ public class PowerUp : MonoBehaviour
         Escudo,
         Velocidad
     }
+
+    [SerializeField] private TipoPowerUp tipo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerPowerUps player = other.GetComponent<PlayerPowerUps>();
+
+        if (player != null)
+        {
+            switch (tipo)
+            {
+                case TipoPowerUp.Curacion:
+                    player.Curar(20f);
+                    break;
+
+                case TipoPowerUp.Escudo:
+                    player.ActivarEscudo(20f);
+                    break;
+
+                case TipoPowerUp.Velocidad:
+                    player.ActivarVelocidad(20f, 3f);
+                    break;
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
